8583f47352b1de4b358013114aeed193a364b82a
aalesv <aalesv70@hotmail.com>
2025-03-07 19:07:55 +0300

    Process case when environment variable not set

--- proxy_exec.cpp
+++ proxy_exec.cpp
@@ -5,18 +5,27 @@
 #include <cstring>
 #include <windows.h>
 
-const char* PROXY_EXEC_CMD = "PROXY_EXEC_CMD";
+const std::string PROXY_EXEC_CMD = "PROXY_EXEC_CMD";
+
+std::string get_env(std::string var)
+{
+	std::string ret = "";
+	char* v = getenv(var.c_str());
+	return v ? v : "";
+}
 
 int main(int argc, char *argv[])
 {
-	const char* cmdToRun = getenv(PROXY_EXEC_CMD);
+	std::string cmdToRun = get_env(PROXY_EXEC_CMD);
+	const std::string myName = argv[0];
+	const std::string fallbackSuffix = ".real.exe";
 	
-	if (cmdToRun == nullptr)
+	if (cmdToRun.length() == 0)
 	{
-		std::cout << argv[0] << " error: "
-		<< "PROXY_EXEC_CMD environment not set, don't know what to run."
+		std::cout << myName << " warning: "
+		<< PROXY_EXEC_CMD << " environment variable not set, trying to guess."
 		<< std::endl;
-		return 1;
+		cmdToRun = myName + fallbackSuffix;
 	}
 
 	std::stringstream stream;    
@@ -24,9 +33,7 @@ int main(int argc, char *argv[])
 
 	LPTSTR cmd  = GetCommandLine();
 
-    int l = strlen(argv[0]);
-
-    if(cmd == strstr(cmd, argv[0]))
+    if(int l = strlen(argv[0]); cmd == strstr(cmd, argv[0]))
     {
         cmd = cmd + l;
         while(*cmd && isspace(*cmd))
